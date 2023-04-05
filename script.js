c4df99e19f75d5d65c2c072946e597203ef6fcfd
Giovanni Manfredi <102854987+John-64@users.noreply.github.com>
2023-04-05 23:10:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 var pomodoro = {
     started : false,
+    onPause : false,
     minutes : 0,
     seconds : 0,
-    fillerHeight : 0,
-    fillerIncrement : 0,
     interval : null,
     minutesDom : null,
     secondsDom : null,
-    fillerDom : null,
+
     init : function(){
       var self = this;
       this.minutesDom = document.querySelector('#minutes');
       this.secondsDom = document.querySelector('#seconds');
-      this.fillerDom = document.querySelector('#filler');
       this.interval = setInterval(function(){
         self.intervalCallback.apply(self);
       }, 1000);
+
       document.querySelector('#work').onclick = function(){
         self.startWork.apply(self);
+        document.getElementById("work").style.display = "none";
+        document.getElementById("pause").style.display = "inline";
       };
+
+      document.querySelector('#pause').onclick = function(){
+        self.pauseWork.apply(self);
+        document.getElementById("pause").style.display = "none";
+        document.getElementById("work").style.display = "inline";
+      };
+
       document.querySelector('#break').onclick = function(){
         self.startBreak.apply(self);
       };
+
       document.querySelector('#stop').onclick = function(){
         self.stopTimer.apply(self);
       };
@@ -30,19 +39,35 @@ var pomodoro = {
       this.minutes = mins;
       this.seconds = secs;
       this.started = started;
-      this.fillerIncrement = 200/(this.minutes*60);
-      this.fillerHeight = 0;  
     },
     startWork: function() {
-      this.resetVariables(50, 0, true);
+        if(this.onPause == true) {
+            this.resetVariables(this.minutes, this.seconds, true);
+            this.onPause = false;
+        } else {
+            this.resetVariables(50, 0, true);
+        }
     },
+    pauseWork: function() {
+        this.onPause = true;
+        this.resetVariables(this.minutes, this.seconds, false);
+      },
     startBreak : function(){
-      this.resetVariables(10, 0, true);
+        document.getElementById("pause").style.display = "none";
+        document.getElementById("work").style.display = "none";
+        this.resetVariables(0, 5, true);
     },
     stopTimer : function(){
-      this.resetVariables(50, 0, false);
-      this.updateDom();
+        document.getElementById("pause").style.display = "none";
+        document.getElementById("work").style.display = "inline";   
+        this.resetVariables(50, 0, false);
+        this.onPause = false;
+        this.updateDom();
     },
+    pauseTimer : function(){
+        this.resetVariables(this.minutes, this.seconds, false);
+        this.updateDom();
+      },
     toDoubleDigit : function(num){
       if(num < 10) {
         return "0" + parseInt(num, 10);
@@ -52,8 +77,6 @@ var pomodoro = {
     updateDom : function(){
       this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
       this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
-      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-      this.fillerDom.style.height = this.fillerHeight + 'px';
     },
     intervalCallback : function(){
       if(!this.started) return false;
@@ -70,8 +93,9 @@ var pomodoro = {
       this.updateDom();
     },
     timerComplete : function(){
-      this.started = false;
-      this.fillerHeight = 0;
+        this.started = false;
+        document.getElementById("pause").style.display = "none";
+        document.getElementById("work").style.display = "inline";   
     }
 };
 window.onload = function(){
