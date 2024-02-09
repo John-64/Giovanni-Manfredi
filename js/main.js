a16389ee191613dd83c5b9d01f770d6ed8de2752
Giovanni Manfredi <manfredi2000@hotmail.it>
2024-02-09 11:09:34 +0100

    Some edit.

--- js/main.js
+++ js/main.js
@@ -8,11 +8,18 @@ function changeModeView() {
         sun.style.display = "none";
         moon.style.display = "block";
         document.body.style.backgroundColor = "var(--clr-light-primary-200)";
+        document.querySelector('.primary-footer').style.color = "var(--clr-light-primary-100)";
         mode = 0;
     } else {
         sun.style.display = "block";
         moon.style.display = "none";
         document.body.style.backgroundColor = "var(--clr-dark-primary-800)";
+        document.querySelector('.primary-footer').style.color = "var(--clr-dark-primary-200)"
         mode = 1;
     }
 }
+
+function showMaintenanceMessage() {
+    const maintenanceMessage = document.getElementById("maintenance-message");
+    maintenanceMessage.style.display = "block";
+  }
